1c1ee4d566c1ed221b70f3078bde982210da2303
mohamedmohamedabdalazez <mohamedmohamedabdalazez2000@gmail.com>
2025-06-10 16:10:03 +0300

    creating temporary database and create movie controller
    # Conflicts:
    #	1.GettingStarted/Movies.Api/Movies.Api.csproj
    #	1.GettingStarted/Movies.Api/Program.cs
    #	1.GettingStarted/Movies.Application/Movies.Application.csproj
    #	RestApiCourse.sln

---
+++ 1.GettingStarted/Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Repositories;
+
+namespace Movies.Api.Controllers;
+[ApiController]
+public class MoviesController : ControllerBase
+{
+   private readonly IMovieRepository _moveRepository;
+
+   public MoviesController(IMovieRepository moveRepository)
+   {
+      _moveRepository = moveRepository;
+   }
+}
